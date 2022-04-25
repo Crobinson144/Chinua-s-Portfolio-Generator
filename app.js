b9caffac93e3541055d19d21f4d081459f4b1996
Crobinson144 <98785033+Crobinson144@users.noreply.github.com>
2022-04-25 19:20:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-const message = 'Hello Node!';
-message = 'Goodbye Node!';
-
-const sum = 5 + 3;
-sum += 1;
-
-const printProfileData = (profileDataArr) => {
-  console.log(profileDataArr);
+const printProfileData = profileDataArr => {
+  for (let i = 0; i < profileDataArr.length; i++) {
+    console.log(profileDataArr[i]);
+  }
 };
-
-printProfileData(profileDataArgs);
-node app.js 'Chinua Robinson' 'Web Developer'
