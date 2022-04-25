ce70994b9ba43b48568aeb9725c314abaaeaaf54
Crobinson144 <98785033+Crobinson144@users.noreply.github.com>
2022-04-25 19:16:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ message = 'Goodbye Node!';
 
 const sum = 5 + 3;
 sum += 1;
+
+const printProfileData = (profileDataArr) => {
+  console.log(profileDataArr);
+};
+
+printProfileData(profileDataArgs);
+node app.js 'Chinua Robinson' 'Web Developer'
