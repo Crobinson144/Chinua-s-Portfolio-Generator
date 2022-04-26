d38bdb2b1ccd34eeed23201d0446467b1502d034
Crobinson144 <98785033+Crobinson144@users.noreply.github.com>
2022-04-25 20:01:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-onsole.log(generatePage('Jane', 'janehub'));
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('(generatePage(name, github');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
+
+const generatePage = () => 'Name: Chinua, Github: ChinuaHUB';
