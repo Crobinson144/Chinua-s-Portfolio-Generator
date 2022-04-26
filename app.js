1117fc67983abf242bad842802ee9e5d9d09d7d9
Crobinson144 <98785033+Crobinson144@users.noreply.github.com>
2022-04-25 20:09:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
-
-
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('(generatePage(Chinua, Github : Chinuahub');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
-
 const generatePage = (userName, githubName) => {
   return `
-    Name: ${userName}
-    GitHub: ${githubName}
+    Name: ${Chinua}
+    GitHub: ${Crobinson144}
   `;
 };
