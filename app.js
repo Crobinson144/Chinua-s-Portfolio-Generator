6c831dcd13786adc902d3acb1a4cf160403949a5
Crobinson144 <98785033+Crobinson144@users.noreply.github.com>
2022-04-25 20:14:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const generatePage = (userName, githubName) => {
+const generatePage = (name, github) => {
   return `
-    Name: ${Chinua}
-    GitHub: ${Crobinson144}
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
   `;
 };
